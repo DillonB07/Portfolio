7226b1e78f8abbd410d75a7c6074ace8e23ad53d
dbarnes18@beaumontleys.leicester.sch.uk <dbarnes18@beaumontleys.leicester.sch.uk>
2021-10-22 17:45:29 +0100

    Add QAPI, Dizzle and flexbox support

--- portfolio/projects.py
+++ portfolio/projects.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Blueprint, render_template, redirect, url_for
 
 projects = Blueprint('projects', __name__)
@@ -10,9 +11,23 @@ def index():
 
 @projects.route('/impulse')
 def impulse():
-    return render_template('projects/impulseProjectPage.html')
+    return render_template('projects/impulse.html')
 
 
 @projects.route('/spotter')
 def spotter():
-    return render_template('projects/spotterProjectPage.html')
+    return render_template('projects/spotter.html')
+
+
+@projects.route('/dizzle')
+def dizzle():
+    url = 'https://qapi-api.ml/'
+    total_questions = requests.get(f'{url}/statistics/total').json()
+    return render_template('projects/dizzle.html', total_questions=total_questions)
+
+
+@projects.route('/qapi')
+def qapi():
+    url = 'https://qapi-api.ml/'
+    total_questions = requests.get(f'{url}/statistics/total').json()
+    return render_template('projects/qapi.html', total_questions=total_questions)
